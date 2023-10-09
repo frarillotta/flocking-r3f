357abeeaa0991d2568d154ed460e178b22ded038
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-10-09 18:26:24 +0100

    make animation faster

--- src/Flock.jsx
+++ src/Flock.jsx
@@ -6,7 +6,7 @@ import { useGLTF } from '@react-three/drei'
 import { MathUtils } from "three";
 let _NUM_BOIDS = 800;
 
-if(window.innerWidth < 500) {
+if (window.innerWidth < 500) {
   _NUM_BOIDS = 250
 }
 
@@ -22,10 +22,12 @@ export function Flock({ airships, player }) {
   });
   const { nodes, materials, animations } = useGLTF('/bird.glb');
 
-  const {ref, actions, names} = useAnimations(animations);
+  const { ref, actions, names } = useAnimations(animations);
 
-  useEffect(( )=> {
-    actions[names[0]].play();
+  useEffect(() => {
+    const animation = actions[names[0]];
+    animation.timeScale = 2.5;
+    animation.play();
   });
 
   return (
@@ -41,17 +43,17 @@ export function Flock({ airships, player }) {
         morphTargetInfluences={nodes.Cylinder.morphTargetInfluences}
       >
         {boids.map(({ position, velocity }, i) => (
-            <Instance
-              scale={[.15, .15, .15]}
-              position={[position.x, position.y, position.z]}
-              rotation={[MathUtils.degToRad(180), MathUtils.degToRad(90) ,MathUtils.degToRad(90)]}
-              velocity={velocity}
-              ref={(el) => {
-                boids[i].ref = el;
-              }}
-              airshipRefs={airships}
-              key={`${i}boid`}
-            />
+          <Instance
+            scale={[.15, .15, .15]}
+            position={[position.x, position.y, position.z]}
+            rotation={[MathUtils.degToRad(180), MathUtils.degToRad(90), MathUtils.degToRad(90)]}
+            velocity={velocity}
+            ref={(el) => {
+              boids[i].ref = el;
+            }}
+            airshipRefs={airships}
+            key={`${i}boid`}
+          />
         ))}
       </Instances>
     </group>
